4fdf15419b9e4801ee825c9e6f82618f12963fc2
Martin <351522453@qq.com>
2015-12-31 13:02:44 +0800

    Update my_test.py
    重新整理下以适应新的提权函数

--- my_test.py
+++ my_test.py
@@ -3,21 +3,22 @@ import my_debugger
 
 debugger = my_debugger.debugger()
 
-debugger.up()
+if debugger.privilege():
+    pid = raw_input('Enter the pid: ')
 
-pid = raw_input('Enter the pid: ')
+    debugger.attach(int(pid))
 
-debugger.attach(int(pid))
+    list_threads = debugger.enumerate_threads()
 
-list_threads = debugger.enumerate_threads()
+    for thread in list_threads:
+        thread_context = debugger.get_thread_context(thread)
+        print thread
+        print 'eax: 0x%08x' %  thread_context.Eax
+        print 'ebx: 0x%08x' %  thread_context.Ebx
+        print 'ecx: 0x%08x' %  thread_context.Ecx
+        print 'edx: 0x%08x' % thread_context.Edx
+        print 'esp: 0x%08x' %  thread_context.Esp
 
-for thread in list_threads:
-    thread_context = debugger.get_thread_context(thread)
-    print thread
-    print 'eax: 0x%08x' %  thread_context.Eax
-    print 'ebx: 0x%08x' %  thread_context.Ebx
-    print 'ecx: 0x%08x' %  thread_context.Ecx
-    print 'edx: 0x%08x' % thread_context.Edx
-    print 'esp: 0x%08x' %  thread_context.Esp
-
-debugger.detach()
+    debugger.detach()
+else:
+    print '退出'
